64897f56c937ae8874efdb2181913d4f701f818a
Benjamin-Yu <ben.yu2@datacom.co.nz>
2024-08-11 13:46:11 +1200

    update modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -11,7 +11,6 @@ function Modal() {
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-md space-y-4 border bg-white p-12">
             <DialogTitle className="font-bold">Help</DialogTitle>
-            <Description>This will permanently deactivate your account</Description>
             <p>Your aim is to capture the pokemon with the targeted ball. <br/>
              Select the ball you would like to move then use the up, down, left, right keys to  <br/>
               move the ball in that direction. 
